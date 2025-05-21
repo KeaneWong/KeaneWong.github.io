9a65ce92365c6849255bda97d387e33bd2eaecc5
Keane Wong <keanewong913@gmail.com>
2025-05-21 01:19:19 -0700

    Main change url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://KeaneWong.github.io/",
   assetsInclude:["**/*.exr"]
 })
